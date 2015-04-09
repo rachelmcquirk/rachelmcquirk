f8608bee7cfee3d85401af766e2b3b2e3b5af544
Rachel McQuirk <rachmcquirk@gmail.com>
2015-04-08 20:42:18 -0700

    digital clock project

---
+++ projects/digitalclock/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+    function displayTime() {
+      var currentTime = new Date();
+      var hours = currentTime.getHours();
+      var minutes = currentTime.getMinutes();
+      var seconds = currentTime.getSeconds();
+      var clock = document.getElementById('clock');
+
+      var meridiem = 'AM';
+      if (hours > 12) {
+        hours = hours - 12;
+        meridiem = 'PM';
+      }
+      if (hours === 0) {
+        hours = 12;
+      }
+
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      }
+      if (hours < 10) {
+        hours = '0' + hours;
+      }
+
+      clock.innerHTML = hours + ':' + minutes + ':' + seconds + ' ' + meridiem;
+
+      if (seconds === '00') {
+        changeColor();
+      }
+    }
+
+
+    function changeColor() {
+      var message = document.getElementById('message');
+      var number = randomColor();
+      document.body.style.backgroundColor = randomColor();
+      message.innerHTML = 'The color of the minute is ' + number;
+    }
+
+    displayTime();
+    changeColor();
+
+    setInterval(displayTime, 1000);
+
+});
